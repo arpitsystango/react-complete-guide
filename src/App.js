fa25853604db78d6e3c3ef89a7d122ca2aa2d3e2
arpit <akparmar@isystango.com>
2018-06-25 15:05:46 +0530

    One way binding/Passing parameters to handler function.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log("Button Clicked!");
     this.setState({
       persons: [
-        { name: "Arpit Parmar", age: 22 },
+        { name: newName, age: 22 },
         { name: "Aditya Parmar", age: 15 },
         { name: "Aditi Soni", age: 21 }
       ]
@@ -27,12 +27,12 @@ class App extends Component {
       <div className="App">
         <h1>I am React Component!</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={() => this.switchNameHandler("this can be inefficient though can be used")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchNameHandler}
+          click={this.switchNameHandler.bind(this, "Use this more often")}
         />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
