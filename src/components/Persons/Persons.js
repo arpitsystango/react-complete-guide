ecaf4d5e3fbbe8624f15c3fac2973074d35d321d
arpit <akparmar@isystango.com>
2018-07-04 16:32:32 +0530

    Made Persons and Person component smart ;P

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => (
-  props.persons.map((person, index) => {
-    return (
-      <Person
-        name={person.name}
-        age={person.age}
-        click={props.clicked.bind(null, index)}
-        key={person.id}
-        changed={props.changed.bind(this, person.id)}
-      />
-    );
-  })
-);
+class Persons extends Component {
+  render() {
+    return this.props.persons.map((person, index) => {
+      return (
+        <Person
+          name={person.name}
+          age={person.age}
+          click={this.props.clicked.bind(null, index)}
+          key={person.id}
+          changed={this.props.changed.bind(this, person.id)}
+        />
+      );
+    });
+  }
+}
 
-export default persons;
+export default Persons;
