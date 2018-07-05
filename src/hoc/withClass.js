2ce4cfc52f25436111a90479c202a9a7357a07c0
arpit <akparmar@isystango.com>
2018-07-05 15:35:09 +0530

    Used anonymous class as the returned value in Higher Order Component.

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const withClass = (ComponentToWrap, className) => {
-  return (props) => (
-    <div className={className}>
-      <ComponentToWrap {...props} />
-    </div>
-  );
+  // return (props) => (
+  //   <div className={className}>
+  //     <ComponentToWrap {...props} />
+  //   </div>
+  // );
+
+  /** We can also return class component using Anonymous Class */
+  return class extends Component {
+    render() {
+      return (
+        <div className={className}>
+          <ComponentToWrap {...this.props} />
+        </div>
+      );
+    }
+  }
 };
 
 export default withClass;
